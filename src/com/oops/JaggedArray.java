cdfe971456cdf0471df6e912bc32c675f9f1c91a
RuchitRK <ruchit2525@gmail.com>
2025-11-26 20:04:43 +0530

    Learned Jagged Array

---
+++ src/com/oops/JaggedArray.java
@@ -0,0 +1,34 @@
+package com.oops;
+
+import java.util.Scanner;
+/*
+ * 123
+ * 1
+ * 12345
+ * 12
+ */
+public class JaggedArray {
+	public static void main(String[] args) {
+		int nrow;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of rows: ");
+		nrow=sc.nextInt();
+		int[][] a=new int[nrow][];
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("Number of columns in a row " +i);
+			a[i]=new int[sc.nextInt()];
+		}
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				a[i][j]=j+1;
+			}
+		}
+		System.out.println("So the output is: ");
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.print(a[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
